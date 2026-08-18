2771cb41e4f4350980c5cd9bacaca50557e6eabc
Hassan Agyemang Boakye <128396960+codedbyhassan@users.noreply.github.com>
2026-08-18 23:32:57 +0000

    refactor: expose runtime architecture boundary

---
+++ src/runtime/index.js
@@ -0,0 +1,6 @@
+export { default as GameRuntime, RUNTIME_STATES } from './GameRuntime';
+export { default as GameClock } from './clock/GameClock';
+export { default as GameInput } from './input/GameInput';
+export { default as GameEventBus } from './events/GameEventBus';
+export { default as EncounterRuntime } from './encounters/EncounterRuntime';
+export { default as CanvasGameRenderer } from './rendering/CanvasGameRenderer';
