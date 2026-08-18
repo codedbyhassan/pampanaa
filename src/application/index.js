3e3ce9709b6114840be176e2768840df05024159
Hassan Agyemang Boakye <128396960+codedbyhassan@users.noreply.github.com>
2026-08-18 23:01:39 +0000

    refactor: expose application boundary

---
+++ src/application/index.js
@@ -0,0 +1,6 @@
+export { appService } from './services/appService';
+export { profileUseCases } from './useCases/profileUseCases';
+export { gameStateUseCases } from './useCases/gameStateUseCases';
+export { achievementUseCases } from './useCases/achievementUseCases';
+export { scoreUseCases } from './useCases/scoreUseCases';
+export { saveUseCases } from './useCases/saveUseCases';
