3635f07a71306d82abee5529ffb78b62a45c6cf1
Hassan Agyemang Boakye <128396960+codedbyhassan@users.noreply.github.com>
2026-08-18 22:56:27 +0000

    refactor: move progress defaults into domain

---
+++ src/domain/progress/defaultProgress.js
@@ -0,0 +1,30 @@
+export const DEFAULT_PROGRESS = Object.freeze({
+  key: 'main',
+  unlockedWeapons: ['blaster'],
+  unlockedSkins: ['default'],
+  selectedSkin: 'default',
+  highestWaveReached: 0,
+  clearedWaves: [],
+  bestScoreByWave: {},
+  totalPlayTime: 0,
+  totalEnemiesDefeated: 0,
+  stats: {
+    totalKillsByType: {
+      Chaser: 0,
+      Shooter: 0,
+      Tank: 0,
+      Swarmer: 0,
+      Splitter: 0,
+      Boss: 0,
+    },
+    totalDeaths: 0,
+    gamesPlayed: 0,
+    shotsFiredByWeapon: {
+      blaster: 0,
+      shotgun: 0,
+      laser: 0,
+      homingMissile: 0,
+      flamethrower: 0,
+    },
+  },
+});
