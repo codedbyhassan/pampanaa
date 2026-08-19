0a709787d37f078193b81d0f912d6a918a4a726f
Hassan Agyemang Boakye <128396960+codedbyhassan@users.noreply.github.com>
2026-08-19 06:22:23 +0000

    feat: expose frontend read model

--- src/application/ui/index.js
+++ src/application/ui/index.js
@@ -1,1 +1,2 @@
 export { FRONTEND_ROUTES, FRONTEND_NAVIGATION, createNavigationState } from './navigationModel';
+export { FRONTEND_MODEL, getMission, getChapter, getNarrativeEvent } from './frontendModel';
